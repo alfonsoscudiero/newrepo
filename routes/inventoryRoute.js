0acf14e6e4071d93579b43b2979e1e2efde67ae9
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-10-14 21:39:11 -0600

    Add route level log

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -7,6 +7,12 @@ const invController = require("../controllers/invController")
 router.get("/type/:classificationId", invController.buildByClassificationId)
 
 // Route to build a single vehicle detail view
-router.get("/detail/:invId", invController.buildVehicleDetail)
+// router.get("/detail/:inv_id", invController.buildVehicleDetail)
+
+// Route to build a single vehicle detail view
+router.get("/detail/:inv_id", (req, res, next) => {
+  console.log("[ROUTE] /inv/detail inv_id =", req.params.inv_id);
+  next();
+}, invController.buildVehicleDetail);
 
 module.exports = router
