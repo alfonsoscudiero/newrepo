4d116ad3fef57d169412d4108fcac85d6f863e43
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-10-21 23:25:21 -0600

    Implement registerAccount controller to process registration form data

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,6 +5,10 @@ Module 05
 // Import utilities to build the nav bar, error handling, etc.
 const utilities = require("../utilities");
 
+// controllers/accountController.js
+const accountModel = require("../models/account-model") // model to save account data
+
+
 /* ****************************************
  *  Deliver login view
  *  Render the login page at GET /account/login
@@ -35,6 +39,47 @@ async function buildRegister(req, res, next) {
   });
 }
 
+/* ****************************************
+ *  Process Registration
+ *  POST route for /account/register
+ * **************************************** */
+async function registerAccount(req, res) {
+  // Build navigation for the layout
+  let nav = await utilities.getNav()
+
+  // Extract data from the form fields
+  const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+  // Send the data to the account model to insert into the DB
+  const regResult = await accountModel.registerAccount(
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+
+  // Handle success/failure directly
+  if (regResult) {
+    // Flash success message using the user's first name
+    req.flash(
+      "notice",
+      `Congratulations, you're registered ${account_firstname}. Please log in.`
+    )
+    // Render login view after successful registration
+    res.status(201).render("account/login", {
+      title: "Login",
+      nav,
+    })
+  } else {
+    // Flash message on registration failure
+    req.flash("notice", "Sorry, the registration failed.")
+    // Re-render the registration page so the user can try again
+    res.status(501).render("account/register", {
+      title: "Registration",
+      nav,
+    })
+  }
+}
 
 //Export controller functions to be used in routes
-module.exports = { buildLogin, buildRegister }
+module.exports = { buildLogin, buildRegister, registerAccount }
