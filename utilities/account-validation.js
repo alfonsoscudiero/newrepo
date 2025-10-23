263f358703405b54414fb3d5257de2bca61f1daa
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-10-22 20:27:38 -0600

    Create account-validation.js with validation rules

---
+++ utilities/account-validation.js
@@ -0,0 +1,50 @@
+// utilities/account-validation.js
+// Required modules
+const utilities = require(".")
+const { body, validationResult } = require("express-validator")
+
+// Create validate object
+const validate = {}
+
+/* ******************************
+ * Registration Data Validation Rules
+ * ****************************** */
+validate.registrationRules = () => {
+  return [
+    // firstname is required and must be string
+    body("account_firstname")
+      .trim()
+      .escape()
+      .notEmpty()
+      .isLength({ min: 1 })
+      .withMessage("Please provide a first name."),
+
+    // lastname is required and must be string
+    body("account_lastname")
+      .trim()
+      .escape()
+      .notEmpty()
+      .isLength({ min: 2 })
+      .withMessage("Please provide a last name."),
+
+    // valid email is required and must not already exist
+    body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required."),
+
+    // password is required and must be strong
+    body("account_password")
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage(
+        "Password must be at least 12 characters and include uppercase, lowercase, number, and special character."
+      ),
+  ]
+}
