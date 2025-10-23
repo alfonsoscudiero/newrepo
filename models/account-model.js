aaf964522cd5549e5558b66208133d20423c6fa0
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-10-23 17:47:58 -0600

    Add checkExistingEmail() to prevent duplicate emails

--- models/account-model.js
+++ models/account-model.js
@@ -25,5 +25,21 @@ async function registerAccount(account_firstname, account_lastname, account_emai
   }
 }
 
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email) {
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1"
+    const email = await pool.query(sql, [account_email])
+    return email.rowCount // 0 means no match; >0 means email exists
+  } catch (error) {
+    return error.message
+  }
+}
+
 // Export model functions
-module.exports = { registerAccount }
+module.exports = {
+  registerAccount,
+  checkExistingEmail,
+}
