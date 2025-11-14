4eb57622d47b065d4c78a3eec387e9173cb7c88c
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-11-14 15:13:53 -0700

    Update edit-inventory.ejs and add JS to enable submit button on input

--- public/js/inv-update.js
+++ public/js/inv-update.js
@@ -3,8 +3,21 @@
  * Module 06 | Week 09
  * Enable the Update button only after some change in the form
  * ******************************************** */
+console.log("inventory.js is RUNNING");
+
 const form = document.querySelector("#updateForm")
-    form.addEventListener("change", function () {
-      const updateBtn = document.querySelector("button")
+
+if (form) {
+  //"input" triggers instantly when the user types or edits.
+  // "change" only triggers after clicking outside the field.
+  form.addEventListener("input", function () {
+    // Look for a submit button inside this form
+    const updateBtn =
+      form.querySelector("button[type='submit']") ||
+      form.querySelector("button")
+
+    if (updateBtn) {
       updateBtn.removeAttribute("disabled")
-    })
+    }
+  })
+}
