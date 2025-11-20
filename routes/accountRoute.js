01f88074389390bdde11995f4a05afb82f88c520
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-11-19 18:30:57 -0700

    Add account update and password change routes

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -63,6 +63,44 @@ router.get(
 )
 
 /* ***************************
+ *  Build Edit Account View
+ *  GET /account/update/:account_id
+ *  Module 06 | Week 10 Task 4 & 5
+ * ************************** */
+router.get(
+  "/update/:account_id",
+  utilities.checkLogin,
+  utilities.handleErrors(accountController.buildUpdateAccount)
+)
+
+/* ***************************
+ *  Process Account Info Update
+ *  POST /account/update
+ *  Module 06 | Week 10 Task 4 & 5
+ * ************************** */
+router.post(
+  "/update",
+  validate.updateAccountRules(), // validate new name + email
+  validate.checkUpdateAccountData, // handle validation errors
+  utilities.checkLogin, // ensure user is still logged in
+  utilities.handleErrors(accountController.updateAccount) // controller to update DB
+)
+
+/* ***************************
+ *  Process Password Change
+ *  POST /account/update-password
+ *  Module 06 | Week 10 Task 4 & 5
+ * ************************** */
+router.post(
+  "/update-password",
+  validate.updatePasswordRules(), // validate new password
+  validate.checkUpdatePasswordData, // handle validation errors
+  utilities.checkLogin, // ensure user is still logged in
+  utilities.handleErrors(accountController.updatePassword) // controller to update password
+)
+
+
+/* ***************************
  *  Export the router so it can be used by server.js
  * ************************** */
 module.exports = router
