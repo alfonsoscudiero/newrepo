8f55dc99d19bee8f4bc22b17f09cef3d59da098a
alfonsoscudiero <alfonsoscudiero@gmail.com>
2025-12-04 21:36:18 -0700

    Add reviewRoute with POST /add endpoint for submitting reviews

---
+++ routes/reviewRoute.js
@@ -0,0 +1,27 @@
+// routes/reviewRoute.js
+
+// Inventory feature routes
+const express = require("express")
+const router = new express.Router()
+
+// Controllers & Utilities
+const utilities = require("../utilities")
+const reviewController = require("../controllers/revController.js") 
+const reviewValidate = require("../utilities/review-validation")
+
+/* ****************************************
+ * Process to add review
+ * POST /reviews/add
+ * Module 07 | Final Project
+ **************************************** */
+router.post(
+  "/add",
+  reviewValidate.reviewRules(),     // 1) run validation rules
+  reviewValidate.checkReviewData,   // 2) check errors & possibly re-render
+  utilities.handleErrors(reviewController.addReview)        // 3) if OK, actually insert into DB
+)
+
+/* ***************************
+ *  Export the router so it can be used by server.js
+ * ************************** */
+module.exports = router
